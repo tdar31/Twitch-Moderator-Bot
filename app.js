2ae4d9c11990845b779b6316c9a13fe8469489b2
tdar31 <t.dardani8@gmail.com>
2018-10-17 01:06:50 -0700

    added slow mode command

--- app.js
+++ app.js
@@ -26,12 +26,18 @@ client.connect();
 client.on('chat', (channel, user, message, self) => {
     // Don't listen to my own messages
     if (self) return;
-    var broadcaster;
+
+    //Assigns channel name to contant
+    var n = channel.split('#')
+    const streamChannel = n[1]
 
     //Assign sender as user/chatter
     let sender = user['display-name']
 
     //Broadcaster is not returned as a mod by default so the 'broadcaster' variable is created to check if the user typing is the broadcaster
+    var broadcaster;
+    //This part must be hardcoded as a bot with more than one channel in the channels prop  will mess this up if you use options.channels
+    //If you use options.channels broadcasters will have mod access on any other channels with access to the bot
     if(user.username == 'test9000channel') {
         broadcaster = true;
     } else {
@@ -40,10 +46,10 @@ client.on('chat', (channel, user, message, self) => {
 
     //Rules message slash test message
     if(message === '!rules') {
-        client.say('test9000channel', 'Rule 1: ---')
-        client.say('test9000channel', 'Rule 2: ---')
-        client.say('test9000channel', 'Rule 3: ---')
-        client.say('test9000channel', 'Rule 4: ---')
+        client.say(streamChannel, 'Rule 1: ---')
+        client.say(streamChannel, 'Rule 2: ---')
+        client.say(streamChannel, 'Rule 3: ---')
+        client.say(streamChannel, 'Rule 4: ---')
     }
 
     //Commands only mods and the broadcaster can use
@@ -51,41 +57,61 @@ client.on('chat', (channel, user, message, self) => {
 
         //Emote only functionality
         if(message === '!emoteonly') {
-            client.emoteonly('test9000channel')
-            client.action('test9000channel', 'The chat has now been set to Emote Only Mode')
+            client.emoteonly(streamChannel)
+            client.action(streamChannel, 'The chat has now been set to Emote Only Mode')
         }
 
         if(message === '!emoteonlyoff') {
-            client.emoteonlyoff('test9000channel')
-            client.action('test9000channel', 'Emote Only Mode has now been turned off')
+            client.emoteonlyoff(streamChannel)
+            client.action(streamChannel, 'Emote Only Mode has now been turned off')
         }
 
         //Follower only functionality
         if(message.includes('!followeronly')) {
             var duration = message.split(' ')
 
+            //Checks to see if duration input is a number.  Sets to 30 mins if invalid or blank
             if(isNaN(duration[1])) {
-                client.followersonly('test9000channel', 30)
-                client.action('test9000channel', 'The chat has now been set to Follower Only Mode')
+                client.followersonly(streamChannel, 30)
+                client.action(streamChannel, 'The chat has now been set to Follower Only Mode')
             } else {
-                client.followersonly('test9000channel', duration[1])
-                client.action('test9000channel', 'The chat has now been set to Follower Only Mode')
+                client.followersonly(streamChannel, duration[1])
+                client.action(streamChannel, 'The chat has now been set to Follower Only Mode')
             }
 
         }
 
         if(message === '!followeronlyoff') {
-            client.followersonlyoff('test9000channel');
-            client.action('test9000channel', 'Followers Only Mode has now been turned off')
+            client.followersonlyoff(streamChannel);
+            client.action(streamChannel, 'Followers Only Mode has now been turned off')
         }
+
+        if(message.includes('!slowmode')) {
+            var duration = message.split(' ')
+
+            //Checks to see if duration input is a number.  Sets to 30 mins if invalid or blank
+            if(isNaN(duration[1])) {
+                client.slow(streamChannel, 30)
+                client.action(streamChannel, 'The chat has now been set to Follower Only Mode')
+            } else {
+                client.slow(streamChannel, duration[1])
+                client.action(streamChannel, 'The chat has now been set to Follower Only Mode')
+            }
+
+        }
+
+        if(message === '!slowmodeoff') {
+            client.slow(streamChannel)
+        }
+
+
+
+
+
+
+
 
     }
 
-    // //Commands that regular chatters can use
-    // if(user['mod'] === false) {
-    //     if(message.includes('www.') || message.includes('.com')){
-    //         client.timeout(channel, sender, 30, 'Link detection system triggered.')
-    //     }
-    // }
 })
 
